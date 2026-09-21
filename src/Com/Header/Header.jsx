20203e4046afb36e41545031c6c637ab46039e56
prernar581-web <prernar581@gmail.com>
2026-09-21 12:18:03 +0530

    fixs

--- src/Com/Header/Header.jsx
+++ src/Com/Header/Header.jsx
@@ -685,7 +685,7 @@ const Header = () => {
                   </a>
 
                   <a
-                    href="https://maps.app.goo.gl/"
+                    href="https://www.google.com/maps/place/Bagh+Ajabgarh/@27.182645,76.2906346,17z/data=!4m9!3m8!1s0x396d832f4cb14005:0x546a721b1294cc8c!5m2!4m1!1i2!8m2!3d27.182645!4d76.2906346!16s%2Fg%2F11m_5kfsy6!5m1!1e1?entry=ttu&g_ep=EgoyMDI2MDkxNi4wIKXMDSoASAFQAw%3D%3D"
                     target="_blank"
                     rel="noreferrer"
                     className="outline-button dark-outline"
